c2dbaacaba6d84c6adfe65d0a9064401c8d7ba7c
JeffersonCarvalhodosSantos <165063096+JeffersonCarvalhodosSantos@users.noreply.github.com>
2024-03-26 14:39:52 -0300

    Update exercicioExtra.py
    Comentários adicionados

--- Curso de Python/Aula 1 - Operadores e Variáveis/exercicioExtra.py
+++ Curso de Python/Aula 1 - Operadores e Variáveis/exercicioExtra.py
@@ -1,15 +1,18 @@
+# Extra 1
 numero = 15
 
 resultado = (numero * numero)
 
 print(resultado)
 
+
+# Extra 2
 palavra1 = input("Digite uma palavra: ")
 palavra2 = input("Digite outra palavra: ")
 
 print("O usuário digitou {palavra1} e {palavra2}")
 
-
+# Extra 3
 numeroInteiro = 50
 antecessor = (numeroInteiro - 1)
 sucessor = (numeroInteiro + 1)
@@ -18,6 +21,7 @@ print(antecessor, numeroInteiro, sucessor)
 
 # Pulando o exercício 4
 
+# Extra 5
 valor = 350
 taxa = 20
 tempo = 30
