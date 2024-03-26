cdeb062260d41e26fcbe1404f7a469947c41976d
JeffersonCarvalhodosSantos <165063096+JeffersonCarvalhodosSantos@users.noreply.github.com>
2024-03-26 16:15:00 -0300

    Aula 2 - Strings
    Aula de strings e suas funções

---
+++ Curso de Python/Aula 2 - Manipulação de String/fatiamentoStrings.py
@@ -0,0 +1,30 @@
+# Capturando a sétima letra (Toda string começa do '0')
+frase = "CURSO DE PYTHON"
+
+setimaLetraFrase = frase[6]
+
+print(setimaLetraFrase)
+
+# Capturando a oitava letra do meu nome
+nome = "Jefferson Carvalho"
+
+oitavaLetraNome = nome[7]
+print(oitavaLetraNome)
+
+# Capturando uma secção    
+
+print(frase[9:15]) # (quando for a última letra, adicionar mais um número, pois ele não reconhece a última como a última)
+
+print(frase[:5]) # Acrescendo um ao final.
+
+print(frase[9:]) # Com os dois pontos automaticamente ele reconhece que tem que ir até o final
+
+print(nome[10:18]) 
+
+### Pulando caracteres
+
+print(frase[9:15:2])  # Terceiro parâmetro como salto (com dois pontos) ou deixar o do meio vazio.
+
+
+
+  
---
+++ Curso de Python/analiseString.py
@@ -0,0 +1,13 @@
+frase = "CURSO DE PYTHON"
+nome = "Jefferson Carvalho"
+
+# Analisar a quantidade da frase: parâmetro LEN
+
+print(len(frase))
+
+print(len(nome))
+
+# Conta quantas vezes a letra "o" aparece 
+
+print(nome.count("o"))
+
