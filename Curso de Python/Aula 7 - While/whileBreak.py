63dc2b758250c2ebbb8d845ad0c8e655199cee0c
JeffersonCarvalhodosSantos <vermelhoabacaxi2023@gmail.com>
2024-04-04 17:04:38 -0300

    While
    Funções do while, break, continue, incluindo Jokenpo

---
+++ Curso de Python/Aula 7 - While/whileBreak.py
@@ -0,0 +1,10 @@
+while True: # Loop infinito
+    
+    
+    continuar = input("Deseja continuar? [S/N]").upper()
+    
+    if continuar == "N":
+        print("Saindo do Loop")
+        break
+    print("Continuando o loop")   # o Break consegue quebrar o Loop
+    
