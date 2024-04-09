06d28cf49e4fef22a0ac0f95e2a831ffd59bae9b
JeffersonCarvalhodosSantos <vermelhoabacaxi2023@gmail.com>
2024-04-09 11:14:04 -0300

    Exercício 4
    Par ou Impar, em casa...

---
+++ Curso de Python/Aula 7 - While/exercicioWhile4.py
@@ -0,0 +1,42 @@
+from random import *
+opcoes = ["IMPAR", "PAR"]
+
+vitorias = 0
+
+
+while True:
+    escolhaMaquina = choice(opcoes)
+    numeroMaquina = randint(0, 10)
+    print(numeroMaquina)
+    print(escolhaMaquina)
+    
+    opcoesUsuario = input("ESCOLHA PAR OU IMPAR: ").upper()
+    numeroUsuario = int(input("Escolha um número de 0 a 10: "))
+
+    soma = numeroMaquina + numeroUsuario
+    parOuImpar = soma % 2
+    
+    
+    if parOuImpar == 0 and opcoesUsuario == "PAR":
+        print(f"Você ganhou, {soma} é par.")
+        vitorias = vitorias + 1
+
+
+    elif parOuImpar == 1 and opcoesUsuario == "IMPAR":
+        print(f"Você ganhou, {soma} é ímpar.")
+        vitorias = vitorias + 1
+
+    elif parOuImpar == 0 and escolhaMaquina == "PAR":
+        print(f"Você perdeu, {soma} é par.")
+        break
+    
+    elif parOuImpar == 1 and escolhaMaquina == "IMPAR":
+        print(f"Você perdeu, {soma} é par.")
+        break
+
+        
+    else:
+        print(f"A OPÇAO ESTÁ INCORRETA")
+        continue
+    
+print(f"Você ganhou {vitorias} seguidas.")
