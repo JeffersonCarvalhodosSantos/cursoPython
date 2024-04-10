4706f9c91f359eefb668d38a16def6592b8698e9
JeffersonCarvalhodosSantos <vermelhoabacaxi2023@gmail.com>
2024-04-10 16:56:22 -0300

    Listas
    Listas exercícios....

---
+++ Curso de Python/Aula 9 - LISTAS/atividadeListas1.py
@@ -0,0 +1,30 @@
+# lista = []
+
+# maior = lista.max
+# menor = lista.max
+# listaOrganizada = lista.sort()
+
+
+
+# for i in range (1,6):
+#     numerosInseridos = int(input(f"Digite o {i}° número: "))
+#     lista.append(numerosInseridos)
+#     print(lista)
+#     print(listaOrganizada)
+
+# print(f"O maior número digitado foi {maior} que está na ")
+
+    
+lista = []
+listaOrganizada = lista.sort()
+
+
+for i in range (1,6):
+    numerosInseridos = int(input(f"Digite o {i}° número: "))
+    lista.append(numerosInseridos)
+    print(lista)
+    print(listaOrganizada)
+    
+    if listaOrganizada # Continuar....
+    
+print(f"O maior número digitado foi {maior} que está na posição {}")  
