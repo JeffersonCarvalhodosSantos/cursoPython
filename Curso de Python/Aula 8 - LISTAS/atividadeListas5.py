e39b6d3006a8f51c9ce1617581dcf4d9bda0dbfc
JeffersonCarvalhodosSantos <vermelhoabacaxi2023@gmail.com>
2024-04-17 20:17:05 -0300

    Update atividadeListas5.py

--- Curso de Python/Aula 8 - LISTAS/atividadeListas5.py
+++ Curso de Python/Aula 8 - LISTAS/atividadeListas5.py
@@ -8,3 +8,22 @@
 # B) Uma listagem com as pessoas mais pesadas.
 
 # C) Um listagem com as pessoas mais leves
+
+lista = []
+pessoas_cadastradas = 0
+
+while True:
+    
+    nome_usuario = input("Digite o nome da pessoa: ").upper()
+    lista.append(nome_usuario)
+    pessoas_cadastradas = pessoas_cadastradas + 1
+    
+    peso_usuario = float(input("Digite o peso da pessoa: ")) 
+    lista.append(peso_usuario)
+    continuar = input("Deseja continuar? S ou N: ").upper()
+    
+    if continuar == "N":
+        break
+    
+print(f"Foram cadastradas {pessoas_cadastradas} pessoas.")
+print(lista)
